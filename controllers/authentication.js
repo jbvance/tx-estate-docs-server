0f8b8e050512e448fda1e816e4f50a838698e960
Jason Vance <jbvance@gmail.com>
2017-05-03 10:26:37 -0500

    modify authentication controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -2,34 +2,31 @@ const jwt = require('jwt-simple');
 const User = require('../models/user');
 const config = require('../config');
 
-function tokenForUser(user){
+function tokenForUser(user) {
   const timestamp = new Date().getTime();
-  return jwt.encode({sub: user.id, iat: timestamp}, config.secret);
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
-exports.signin = (req, res, next) => {
+exports.signin = function(req, res, next) {
   // User has already had their email and password auth'd
- // We just need to give them a token
- res.send({ token: tokenForUser(req.user) });
-};
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
 
-exports.signup = function (req, res, next) {
-
+exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
-  if(!email || !password){
-    return res.status(422).send({error: 'You must provide email and password'});
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password'});
   }
 
-  //See if a user with the given email exists
-  User.findOne({email: email}, (err, existingUser) => {
-    if (err) { return next(err);}
+  // See if a user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
-    // If a user does exist, return an error
-
+    // If a user with email does exist, return an error
     if (existingUser) {
-      //http status code 422 is unprocessable entity
       return res.status(422).send({ error: 'Email is in use' });
     }
 
@@ -37,14 +34,13 @@ exports.signup = function (req, res, next) {
     const user = new User({
       email: email,
       password: password
-    })
-    user.save((err) => {
+    });
+
+    user.save(function(err) {
       if (err) { return next(err); }
-    }); //Must call save to save record to database
 
-      //Respond to request indicating user was created
+      // Repond to request indicating the user was created
       res.json({ token: tokenForUser(user) });
-
-  }); //end of callback function
-
-};
+    });
+  });
+}
